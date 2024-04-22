5ebc074fd9a2b6b6b6a12fdec0a98fdf6da590ad
voidrak <112881704+voidrak@users.noreply.github.com>
2024-04-22 14:25:20 +0300

    some progress on nav bar

--- src/Components/SharedLayout/NavigationBar.jsx
+++ src/Components/SharedLayout/NavigationBar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import { motion, AnimatePresence } from "framer-motion";
 const NavigationBar = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
   const navBarLink = [
@@ -8,21 +8,79 @@ const NavigationBar = () => {
     { title: "Project", href: "#" },
     { title: "Contact", href: "#" },
   ];
+
+  const menuSlide = {
+    initial: {
+      x: "100%",
+    },
+    enter: {
+      x: "0%",
+      transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1] },
+    },
+    exit: {
+      x: "100%",
+      transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1] },
+    },
+  };
+
+  const slide = {
+    initial: {
+      x: "80px",
+    },
+    enter: (i) => ({
+      x: "0px",
+      transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1], delay: 0.07 * i },
+    }),
+    exit: (i) => ({
+      x: "80px",
+      transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1], delay: 0.07 * i },
+    }),
+  };
+
   return (
-    <div className="fixed right-0 top-0 z-[9999] ">
+    <div className="fixed right-0 top-0 z-50 ">
       <div
         onClick={() => {
           setIsNavOpen(!isNavOpen);
         }}
-        className=" fixed right-2 top-0 m-[20px] flex h-[80px]  w-[80px] items-center justify-center rounded-full bg-first-accent-color "
+        className=" fixed right-2 top-0 z-40 m-[20px] flex  h-[80px] w-[80px] items-center justify-center rounded-full bg-first-accent-color "
       >
         <div
-          className={`w-full bg-transparent before:relative before:top-[5px] before:m-auto  before:block before:h-[2px] before:w-[40%] before:bg-white after:relative after:top-[-5px]  after:m-auto after:block after:h-[2px] after:w-[40%] after:bg-white ${isNavOpen ? "before:top-[-1px] before:rotate-[-45deg]  after:rotate-[45deg]" : ""}  `}
+          className={`w-full bg-transparent before:relative before:top-[5px] before:m-auto  before:block before:h-[2px] before:w-[40%] before:bg-white after:relative after:top-[-5px]  after:m-auto after:block after:h-[2px] after:w-[40%] after:bg-white ${isNavOpen ? "before:top-[-1px]  before:rotate-[-45deg] after:top-[1px]  after:rotate-[45deg]" : ""} before:transition-all before:duration-200  after:transition-all after:duration-200 `}
         ></div>
       </div>
-      {isNavOpen && (
-        <div className="h-[100vh] w-[100vw] max-w-[481px] bg-black  "></div>
-      )}
+      <AnimatePresence mode="wait">
+        {isNavOpen && (
+          <motion.div
+            variants={menuSlide}
+            initial="initial"
+            animate="enter"
+            exit="exit"
+            className="h-[100vh] w-[100vw] max-w-[481px] bg-black  text-text-color-bold "
+          >
+            <div className="body  flex justify-between p-[100px]">
+              <div className="nav mt-20 flex w-full flex-col gap-4 text-[56px]">
+                <div className="header border-b border-text-color-light text-base  ">
+                  <p className="pb-2 text-text-color-light">Navigation</p>
+                </div>
+                {navBarLink.map((item, index) => (
+                  <motion.a
+                    custom={index}
+                    variants={slide}
+                    initial="initial"
+                    animate="enter"
+                    exit="exit"
+                    href={item.href}
+                    className=" link font-[300] first-letter:uppercase"
+                  >
+                    {item.title}
+                  </motion.a>
+                ))}
+              </div>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
