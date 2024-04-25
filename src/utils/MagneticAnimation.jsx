56d1cec9e3eb8c8670fecbf0e0a7e32ab099af03
voidrak <112881704+voidrak@users.noreply.github.com>
2024-04-25 19:32:08 +0300

    add the tech stack

---
+++ src/utils/MagneticAnimation.jsx
@@ -0,0 +1,34 @@
+import React, { useRef, useState } from "react";
+import { motion } from "framer-motion";
+const MagneticAnimation = ({ children }) => {
+  const ref = useRef(null);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  const mouseMove = (e) => {
+    const { clientX, clientY } = e;
+    const { width, height, left, top } = ref.current.getBoundClientRect();
+    const x = clientX - (left - width / 2);
+    const y = clientY - (top - width / 2);
+    setPosition({ x, y });
+  };
+  const mouseLeave = (e) => {
+    setPosition({ x: 0, y: 0 });
+  };
+
+  const { x, y } = position;
+
+  return (
+    <motion.div
+      onMouseMove={mouseMove}
+      onMouseLeave={mouseLeave}
+      ref={ref}
+      animate={{ x, y }}
+      transition={{ type: "spring", stiffness: 150, damping: 15, mass: 0.1 }}
+      className="border border-orange-300"
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default MagneticAnimation;
