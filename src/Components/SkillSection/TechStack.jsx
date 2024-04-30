fb58b46d14eb22c2fe62c1a967b729a7c563dd4f
voidrak <112881704+voidrak@users.noreply.github.com>
2024-05-01 00:32:59 +0300

    add custom cursor to tech stack section

--- src/Components/SkillSection/TechStack.jsx
+++ src/Components/SkillSection/TechStack.jsx
@@ -36,7 +36,7 @@ const TechStack = () => {
     },
   };
   return (
-    <div ref={container} id="skills" className="    ">
+    <div ref={container} id="skills" className=" tech-stack   ">
       <Reveal>
         <div className="  relative  mx-auto w-[250px] overflow-x-hidden bg-transparent  pb-24  text-4xl font-bold text-text-color-bold  ">
           TECH STACK
@@ -62,7 +62,7 @@ const TechStack = () => {
                 <img
                   src="https://img.shields.io/badge/javascript-%23323330.svg?logo=javascript&logoColor=%23F7DF1E"
                   alt="JavaScript"
-                  className=" mx-11  my-5 scale-x-[1.8] scale-y-[2] cursor-pointer "
+                  className=" mx-11  my-5 scale-x-[1.8] scale-y-[2]  "
                 />
               </MagneticAnimation>
 
@@ -70,35 +70,35 @@ const TechStack = () => {
                 <img
                   src=" https://img.shields.io/badge/C++-%2300599C.svg?logo=c%2B%2B&logoColor=white "
                   alt=" c++ "
-                  className=" mx-11  my-5 scale-x-[1.8] scale-y-[2] cursor-pointer "
+                  className=" mx-11  my-5 scale-x-[1.8] scale-y-[2]  "
                 />
               </MagneticAnimation>
               <MagneticAnimation>
                 <img
                   src=" https://img.shields.io/badge/Css-%231572B6.svg?logo=css3&logoColor=white "
                   alt="css3  "
-                  className=" mx-11  my-5 scale-x-[1.8] scale-y-[2] cursor-pointer "
+                  className=" mx-11  my-5 scale-x-[1.8] scale-y-[2]  "
                 />
               </MagneticAnimation>
               <MagneticAnimation>
                 <img
                   src="https://img.shields.io/badge/HTML5-%23E34F26.svg?logo=html5&logoColor=white  "
                   alt=" html "
-                  className=" mx-11  my-5 scale-x-[1.8] scale-y-[2] cursor-pointer "
+                  className=" mx-11  my-5 scale-x-[1.8] scale-y-[2]  "
                 />
               </MagneticAnimation>
               <MagneticAnimation>
                 <img
                   src="  https://img.shields.io/badge/JAVA-%23ED8B00.svg?logo=openjdk&logoColor=white"
                   alt=" java "
-                  className=" mx-11  my-5 scale-x-[1.8] scale-y-[2] cursor-pointer "
+                  className=" mx-11  my-5 scale-x-[1.8] scale-y-[2]  "
                 />
               </MagneticAnimation>
               <MagneticAnimation>
                 <img
                   src=" https://img.shields.io/badge/Markdown-%23000000.svg?logo=markdown&logoColor=white "
                   alt="Markdown  "
-                  className=" mx-11  my-5 scale-x-[1.8] scale-y-[2] cursor-pointer "
+                  className=" mx-11  my-5 scale-x-[1.8] scale-y-[2]  "
                 />
               </MagneticAnimation>
             </div>
@@ -111,14 +111,14 @@ const TechStack = () => {
                 <img
                   src="https://img.shields.io/badge/React-%2320232a.svg?style=plastic&logo=react&logoColor=%2361DAFB"
                   alt="react"
-                  className=" mx-11  my-5 scale-x-[1.8] scale-y-[2] cursor-pointer "
+                  className=" mx-11  my-5 scale-x-[1.8] scale-y-[2]  "
                 />
               </MagneticAnimation>
               <MagneticAnimation>
                 <img
                   src=" https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=plastic&logo=tailwind-css&logoColor=white "
                   alt=" tailwindcss "
-                  className=" mx-11  my-5 scale-x-[1.8] scale-y-[2] cursor-pointer "
+                  className=" mx-11  my-5 scale-x-[1.8] scale-y-[2]  "
                 />
               </MagneticAnimation>
             </div>
@@ -130,21 +130,21 @@ const TechStack = () => {
                 <img
                   src="https://img.shields.io/badge/Microsoft%20SQL%20Server-CC2927?style=plastic&logo=microsoft%20sql%20server&logoColor=white"
                   alt="Microsoft Server "
-                  className=" mx-11  my-5 scale-x-[1.8] scale-y-[2] cursor-pointer "
+                  className=" mx-11  my-5 scale-x-[1.8] scale-y-[2]  "
                 />
               </MagneticAnimation>
               <MagneticAnimation>
                 <img
                   src=" https://img.shields.io/badge/mysql-%2300000f.svg?style=plastic&logo=mysql&logoColor=white "
                   alt=" mysql "
-                  className=" mx-11  my-5 scale-x-[1.8] scale-y-[2] cursor-pointer "
+                  className=" mx-11  my-5 scale-x-[1.8] scale-y-[2]  "
                 />
               </MagneticAnimation>
               <MagneticAnimation>
                 <img
                   src=" https://img.shields.io/badge/Firebase-039BE5?style=plastic&logo=Firebase&logoColor=white "
                   alt="Firebase  "
-                  className=" mx-11  my-5 scale-x-[1.8] scale-y-[2] cursor-pointer "
+                  className=" mx-11  my-5 scale-x-[1.8] scale-y-[2]  "
                 />
               </MagneticAnimation>
             </div>
@@ -156,14 +156,14 @@ const TechStack = () => {
                 <img
                   src="https://img.shields.io/badge/git-%23F05033.svg?style=plastic&logo=git&logoColor=white"
                   alt="git "
-                  className=" mx-11  my-5 scale-x-[1.8] scale-y-[2] cursor-pointer "
+                  className=" mx-11  my-5 scale-x-[1.8] scale-y-[2]  "
                 />
               </MagneticAnimation>
               <MagneticAnimation>
                 <img
                   src="https://img.shields.io/badge/github-%23121011.svg?style=plastic&logo=github&logoColor=white "
                   alt=" github "
-                  className=" mx-11  my-5 scale-x-[1.8] scale-y-[2] cursor-pointer "
+                  className=" mx-11  my-5 scale-x-[1.8] scale-y-[2]  "
                 />
               </MagneticAnimation>
             </div>
@@ -175,14 +175,14 @@ const TechStack = () => {
                 <img
                   src="https://img.shields.io/badge/Adobe%20XD-470137?style=plastic&logo=Adobe%20XD&logoColor=#FF61F6"
                   alt=" Adobe "
-                  className=" mx-11  my-5 scale-x-[1.8] scale-y-[2] cursor-pointer "
+                  className=" mx-11  my-5 scale-x-[1.8] scale-y-[2]  "
                 />
               </MagneticAnimation>
               <MagneticAnimation>
                 <img
                   src="https://img.shields.io/badge/figma-%23F24E1E.svg?style=plastic&logo=figma&logoColor=white "
                   alt="figma  "
-                  className=" mx-11  my-5 scale-x-[1.8] scale-y-[2] cursor-pointer "
+                  className=" mx-11  my-5 scale-x-[1.8] scale-y-[2]  "
                 />
               </MagneticAnimation>
             </div>
