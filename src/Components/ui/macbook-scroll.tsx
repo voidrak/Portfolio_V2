b6a5dd2d9cd0ac3defabf80f58c5ac0986cb6caf
voidrak <112881704+voidrak@users.noreply.github.com>
2024-05-01 15:15:24 +0300

    fixed the overflow problem in the hero section

--- src/Components/ui/macbook-scroll.tsx
+++ src/Components/ui/macbook-scroll.tsx
@@ -87,7 +87,7 @@ export const MacbookScroll = ({
   return (
     <div
       ref={ref}
-      className="overflowY-hidden  flex min-h-[100vh] flex-shrink-0 scale-[0.5] transform  flex-col items-center justify-start from-slate-900 via-slate-800 to-blue-900 py-0 [perspective:800px] sm:scale-50 md:min-h-[200vh] md:translate-y-[-10rem] md:scale-100  md:bg-gradient-to-br md:py-80  "
+      className="debug flex min-h-[100vh] flex-shrink-0 scale-[0.5] transform flex-col  items-center justify-start overflow-hidden from-slate-900 via-slate-800 to-blue-900 py-0 [perspective:800px] sm:scale-50 md:min-h-[200vh] md:translate-y-[-10rem] md:scale-100  md:bg-gradient-to-br md:py-80  "
     >
       <motion.div
         style={{
@@ -674,27 +674,6 @@ export const OptionKey = ({ className }: { className: string }) => {
         width="32"
         height="32"
         stroke="none"
-      />
-    </svg>
-  );
-};
-
-const AceternityLogo = () => {
-  return (
-    <svg
-      width="66"
-      height="65"
-      viewBox="0 0 66 65"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      className="h-3 w-3 text-white"
-    >
-      <path
-        d="M8 8.05571C8 8.05571 54.9009 18.1782 57.8687 30.062C60.8365 41.9458 9.05432 57.4696 9.05432 57.4696"
-        stroke="currentColor"
-        strokeWidth="15"
-        strokeMiterlimit="3.86874"
-        strokeLinecap="round"
       />
     </svg>
   );
